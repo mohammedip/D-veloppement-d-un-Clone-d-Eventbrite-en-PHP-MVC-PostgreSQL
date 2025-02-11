755bbf00bff1bb3d13364d33b9a1f4b2f1283660
Mohammed reida <reidamohammed3@gmail.com>
2025-02-11 17:01:18 +0100

    fix merge problem

--- app/views/back-office/watch-video.php
+++ app/views/back-office/watch-video.php
@@ -15,61 +15,13 @@
 </head>
 <body>
 
-<header class="header">
-   
-   <section class="flex">
+  <!-- start header  -->
+  <?php require_once "header.php" ?>
+  <!-- end header  -->
 
-      <a href="home.html" class="logo">Educa.</a>
-
-      <form action="search.html" method="post" class="search-form">
-         <input type="text" name="search_box" required placeholder="search courses..." maxlength="100">
-         <button type="submit" class="fas fa-search"></button>
-      </form>
-
-      <div class="icons">
-         <div id="menu-btn" class="fas fa-bars"></div>
-         <div id="search-btn" class="fas fa-search"></div>
-         <div id="user-btn" class="fas fa-user"></div>
-         <div id="toggle-btn" class="fas fa-sun"></div>
-      </div>
-
-      <div class="profile">
-         <img src="images/pic-1.jpg" class="image" alt="">
-         <h3 class="name">shaikh anas</h3>
-         <p class="role">studen</p>
-         <a href="profile.html" class="btn">view profile</a>
-         <!-- <div class="flex-btn">
-            <a href="login.html" class="option-btn">login</a>
-            <a href="register.html" class="option-btn">register</a>
-         </div> -->
-      </div>
-
-   </section>
-
-</header>   
-
-<div class="side-bar">
-
-   <div id="close-btn">
-      <i class="fas fa-times"></i>
-   </div>
-
-   <div class="profile">
-      <img src="images/pic-1.jpg" class="image" alt="">
-      <h3 class="name">shaikh anas</h3>
-      <p class="role">studen</p>
-      <a href="profile.html" class="btn">view profile</a>
-   </div>
-
-   <nav class="navbar">
-      <a href="home.html"><i class="fas fa-home"></i><span>home</span></a>
-      <a href="about.html"><i class="fas fa-question"></i><span>about</span></a>
-      <a href="courses.html"><i class="fas fa-graduation-cap"></i><span>courses</span></a>
-      <a href="teachers.html"><i class="fas fa-chalkboard-user"></i><span>teachers</span></a>
-      <a href="contact.html"><i class="fas fa-headset"></i><span>contact us</span></a>
-   </nav>
-
-</div>
+<!-- side bar start  -->
+ <?php require_once "sideBar.php" ?>
+<!-- side bar end  -->
 
 <section class="watch-video">
 
@@ -209,11 +161,9 @@
 
 
 
-<footer class="footer">
-
-   &copy; copyright @ 2022 by <span>mr. web designer</span> | all rights reserved!
-
-</footer>
+<!-- start footer -->
+<?php require_once "footer.php" ?>
+<!-- end footer -->
 
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
